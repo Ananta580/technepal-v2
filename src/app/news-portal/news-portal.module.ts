3f46fc8e6fccb69507a10365f4f32a1e2f51e57c
ananta580 <anantapoudel580@gmail.com>
2023-05-11 04:51:11 +0545

    Code push

--- src/app/news-portal/news-portal.module.ts
+++ src/app/news-portal/news-portal.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { NewsPortalRoutingModule } from './news-portal-routing.module';
 import { MainComponent } from './pages/main/main.component';
 import { NewsPortalComponent } from './news-portal.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [MainComponent, NewsPortalComponent],
+  declarations: [MainComponent, NewsPortalComponent, NavBarComponent],
   imports: [CommonModule, NewsPortalRoutingModule],
 })
 export class NewsPortalModule {}
--- src/app/news-portal/pages/main/main.component.ts
+++ src/app/news-portal/pages/main/main.component.ts
@@ -2,6 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
+  styles: [
+    `
+      .top-category {
+        overflow-x: scroll;
+        overflow-y: hidden;
+      }
+
+      .top-category::-webkit-scrollbar {
+        display: none;
+      }
+
+      .top-category {
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+      }
+    `,
+  ],
   templateUrl: './main.component.html',
 })
 export class MainComponent {}
