0076aa460361cc5d6e56964f2a1005961171cd94
ananta580 <anantapoudel580@gmail.com>
2023-05-27 00:19:15 +0545

    Commited Admin blog

--- src/app/news-admin-portal/pages/news-lists/news-lists.module.ts
+++ src/app/news-admin-portal/pages/news-lists/news-lists.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { NewsListsRoutingModule } from './news-lists-routing.module';
 import { NewsListsComponent } from './news-lists.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [NewsListsComponent],
-  imports: [CommonModule, NewsListsRoutingModule],
+  imports: [CommonModule, SharedModule, NewsListsRoutingModule],
 })
 export class NewsListsModule {}
--- src/app/news-portal/pages/news-detail/news-detail.component.ts
+++ src/app/news-portal/pages/news-detail/news-detail.component.ts
@@ -1,20 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { News } from '../main/shared/models/news.model';
 import { HotNews } from '../main/shared/mockup';
+import { BlogService } from 'src/app/news-admin-portal/shared/services/blog.service';
+import { ApiResonse } from 'src/app/shared/model/api.response';
+import { BlogBE } from 'src/app/news-admin-portal/shared/model/blog';
 
 @Component({
   selector: 'app-news-detail',
   templateUrl: './news-detail.component.html',
 })
-export class NewsDetailComponent {
-  blog?: News;
+export class NewsDetailComponent implements OnInit {
+  blog?: BlogBE;
   combinedTitle = '';
 
-  constructor(private _route: ActivatedRoute) {
+  constructor(
+    private _route: ActivatedRoute,
+    private blogService: BlogService
+  ) {
     this.combinedTitle = _route.snapshot.paramMap.get('name') ?? '';
-    console.log(this.combinedTitle);
-    this.blog = HotNews.find((x) => x.combinedTitle == this.combinedTitle)!;
-    console.log(this.blog);
+  }
+
+  ngOnInit(): void {
+    this.loadBlog();
+  }
+
+  loadBlog() {
+    this.blogService.getBlogByCombinedTitle(this.combinedTitle).subscribe({
+      next: (res: ApiResonse<BlogBE>) => {
+        this.blog = res.data;
+      },
+    });
   }
 }
